162bb482128aa631fef22b98dc39590d6c28bf02
Saurabh Kumar Singh <imsaurabhsinha51@gmail.com>
2025-08-28 23:38:28 +0530

    util codes

--- src/index.js
+++ src/index.js
@@ -1,9 +1,22 @@
 import connectDB from "./db/index.js";
 import dotenv from "dotenv";
+import express from "express";
+
+const app = express();
+
 dotenv.config({
     path: './env'
 });
-connectDB();
+
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running on port ${process.env.PORT}`);
+    });
+})
+.catch((err) =>{
+    console.log(err);
+})
 
 // import express from express;
 // const app = express();
